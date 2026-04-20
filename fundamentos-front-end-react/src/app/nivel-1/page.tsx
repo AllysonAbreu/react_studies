8d85eec0a789cf78b83852f78393f37aca94828d
allyson <allyson.abreu@cwi.com.br>
2026-04-20 17:10:06 -0300

    feat: Conceito e utilização de estado do cliente pelo servidor

---
+++ fundamentos-front-end-react/src/app/nivel-1/page.tsx
@@ -0,0 +1,13 @@
+import { Contador } from "@/components/Contador";
+import Link from "next/link";
+
+const Page = ()  => (
+  <div>
+    <h1 className="text-4xl font-bold">Página Nível 1</h1>
+    <Contador/>
+  </div>
+);
+
+export default Page;
+
+
